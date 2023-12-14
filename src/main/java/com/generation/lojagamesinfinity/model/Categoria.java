c0cd913cd4ce98ddc9d05d9c2ffd0eb14a7763d9
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-14 17:31:11 -0300

    add relacionamento com a classe Produtos

--- src/main/java/com/generation/lojagamesinfinity/model/Categoria.java
+++ src/main/java/com/generation/lojagamesinfinity/model/Categoria.java
@@ -1,10 +1,18 @@
 package com.generation.lojagamesinfinity.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+																																																																																										
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -19,9 +27,14 @@ public class Categoria {
 	private Long id;
 
 	@NotBlank(message = "O atributo nome categoria é obrigatorio !!")
-	@Size(min = 5, max = 100, message = "O atributo nome categoria deve conter no mínimo 05 e no máximo 100 caracteres")
+	@Size(min = 3, max = 100, message = "O atributo nome categoria deve conter no mínimo 05 e no máximo 100 caracteres")
 	@Column(length = 100)
 	private String nome;
+	
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produtos> produtos;
 
 	public Long getId() {
 		return id;
@@ -37,6 +50,13 @@ public class Categoria {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setPostagem(List<Produtos> produtos) {
+		this.produtos = produtos;
 	}
 
 }
