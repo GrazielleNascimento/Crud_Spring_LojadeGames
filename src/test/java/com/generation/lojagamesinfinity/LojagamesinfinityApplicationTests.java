38f4e029126895ce3bbde90ba37d403b3f0e4c0c
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-14 11:37:40 -0300

    add aplicationTests

---
+++ src/test/java/com/generation/lojagamesinfinity/LojagamesinfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.lojagamesinfinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojagamesinfinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
