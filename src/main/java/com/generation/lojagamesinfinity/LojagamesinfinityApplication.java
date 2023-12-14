976388832c86ae1b2d007e9f68c194e57eea5769
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-14 11:37:17 -0300

    add aplic

---
+++ src/main/java/com/generation/lojagamesinfinity/LojagamesinfinityApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojagamesinfinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojagamesinfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojagamesinfinityApplication.class, args);
+	}
+
+}
